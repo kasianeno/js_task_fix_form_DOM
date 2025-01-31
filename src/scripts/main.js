d558f2fa6213e415e39b3503ef12072f36885623
Oleksandr <dedikad@gmail.com>
2025-01-31 16:18:31 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const inputs = document.querySelectorAll('form input');
 
-  inputs.forEach(((input) => {
-    const parentelement = input.parentElement;
+  inputs.forEach((input) => {
+    const parentElement = input.parentElement;
     const label = document.createElement('label');
 
     label.classList.add('field-label');
     label.setAttribute('for', input.id);
-    label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
     input.setAttribute('placeholder', label.textContent);
 
-    parentelement.insertBefore(label, input);
-  }))
-})
+    parentElement.insertBefore(label, input);
+  });
+});
